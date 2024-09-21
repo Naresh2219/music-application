3a33e42729e96e5fb34791d7da3dfeee62816528
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2024-09-21 22:07:05 +0530

    Update server.js

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(cors({
     origin:'*',
-    methods:['GET', 'POST'],
+    methods:['GET', 'POST', 'OPTIONS'],
 }))
 app.use(express.json({ limit: '10mb' })); // Set limit for JSON requests
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // Set limit for URL-encoded data
